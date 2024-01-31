1d38508e887cbb91b01bb70609a7818f75f23286
KARTHICK877 <kmass8754@gmail.com>
2024-01-31 14:56:38 +0530

    up

--- server/db.js
+++ server/db.js
@@ -4,15 +4,28 @@ import dotenv from "dotenv";
 dotenv.config();
 const connectionString = process.env.CONNECTION_URL;
 
-async function mongoConnection() {
+const mongoConnection = async () => {
   const client = new MongoClient(connectionString);
 
-  await client.connect();
+  try {
+    await client.connect();
+    console.log("Database connected successfully");
+    return client;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error; // Re-throw the error to indicate a failed connection
+  }
+};
 
-  console.log("Database connected successfully");
+// Export a promise that resolves to the connected client
+export const client = mongoConnection();
 
-  return client;
-}
+// Example usage in another file:
+// import { clientPromise } from "./yourDatabaseModule";
+// (async () => {
+//   const client = await clientPromise;
+//   // Now you can use the connected client
+// })();
 
 //initializing DB
-export const client = await mongoConnection();
+// export const client = await mongoConnection();
